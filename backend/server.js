349401a4f7e2c55b20c221c2010fd81067629b81
IulianBotnari <iulianbotnari93@gmail.com>
2025-01-31 14:05:11 +0100

    [WIP] basic server done

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const server = express();
+const dotenv = require('dotenv');
 server.use(express.json())
 server.use(cors())
+dotenv.config();
+const host = process.env.HOST
+const port = process.env.PORT
+
+
+server.listen(port, () => {
+    console.log(`Server running at http://${host}:${port}`)
+})
