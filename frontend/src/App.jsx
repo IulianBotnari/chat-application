3f2f53c52eb296ecf8d22821e72fc7e655f1fe9a
IulianBotnari <iulianbotnari93@gmail.com>
2025-01-31 10:04:10 +0100

    [WIP] First basic frontend structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import LogInPage from './pages/LogIn/LogInPage.jsx'
+import SignUpPage from './pages/SignUp/SignUpPage.jsx'
+import HomePage from './pages/HomePage/HomePage.jsx'
+import { BrouserRouter, Routes, Router } from 'react-router';
+
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
