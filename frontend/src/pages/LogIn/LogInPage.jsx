962e26427e7f403cb83930ea6e200dcf3b5d9c2a
IulianBotnari <iulianbotnari93@gmail.com>
2025-01-31 13:13:02 +0100

    [WIP] add login page

--- frontend/src/pages/LogIn/LogInPage.jsx
+++ frontend/src/pages/LogIn/LogInPage.jsx
@@ -1,9 +1,17 @@
-
+import style from './Login.module.css'
 
 export default function LogInPage() {
     return (
-        <>
+        <div className={style.login_container}>
 
-        </>
+
+            <form className={style.card}>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+                <p>Don't have an account? <a href="/register">Register</a></p>
+            </form>
+
+        </div>
     )
 }
